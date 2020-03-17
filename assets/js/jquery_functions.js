fa83a02fd11cba5ce6a9667934c7244ec4b06a6c
davien21 <chidiebereekennia@gmail.com>
2020-03-17 19:58:55 +0100

    removed owl slider

--- assets/js/jquery_functions.js
+++ assets/js/jquery_functions.js
@@ -48,19 +48,6 @@ $(document).ready(function(){
 	});
 	
 
- 
-  $("#owl-demo").owlCarousel({
- 
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
- 
-      items : 4,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3],
-      itemsTablet: [600,2], //2 items between 600 and 0
-      // itemsMobile : [500,1] // itemsMobile disabled - inherit from itemsTablet option
- 
-  });
-
 
      
 
