5c8a644512f380af50ce79ef6636ca9d49424579
davien21 <chidiebereekennia@gmail.com>
2020-03-17 19:38:38 +0100

    Added events section

--- assets/js/jquery_functions.js
+++ assets/js/jquery_functions.js
@@ -46,7 +46,21 @@ $(document).ready(function(){
 		$('#side-menu-nav').removeClass('show-sidebar');
 		hideSideBar();
 	});
-		
+	
+
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 3,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+     
+
+	
 	  
 	// $('#what-we-do a.get-claired').mouseenter(function(){
 	// 	$('#what-we-do a.get-claired .animated').addClass('bounce infinite')
